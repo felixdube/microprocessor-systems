79ba520d274295a40a1a1dce4fc6e0d643e145f4
Felix Dube <felix.dube@mail.mcgill.ca>
2016-02-02 15:15:14 -0500

    Add part 3

--- Lab1_STM32F4Cube_Base_project/KalmanFilter.c
+++ Lab1_STM32F4Cube_Base_project/KalmanFilter.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 #include "KalmanFilter.h"
 
 int Kalmanfilter_C(float* InputArray, float* OutputArray, struct kalman_state* kstate, int Length) {
@@ -14,4 +15,60 @@ int Kalmanfilter_C(float* InputArray, float* OutputArray, struct kalman_state* k
 //		printf("%f\n", kstate->x);
 	}
 	return 0;
+}
+
+void home_sub_f32 (float *inputA, float *inputB, float *output, int length) {
+	int i;
+	for(i = 0; i < length; i++) {
+		output[i] = inputA[i] - inputB[i];
+	}
+}
+
+void home_std_f32 (float *input, int length, float *output){
+	int i;
+	float sumOfSquare = 0;
+	float sum = 0;
+	
+	for(i = 0; i < length; i++){
+		sumOfSquare = sumOfSquare + input[i]*input[i];
+	}
+	
+	for (i = 0; i < length; i++) {
+		sum = sum + input[i];
+	}
+	
+	*output = sqrt((sumOfSquare - sum*sum/length) / (length -1));
+}
+
+void home_mean_f32 (float *input, int length, float *output) {
+	int i;
+	float sum = 0;
+	
+	for (i = 0; i < length; i++) {
+		sum = sum + input[i];
+	}
+	
+	*output = sum/length;
+}
+
+void home_correlate_f32 (float *inputA, float *inputB, int length, float *output) {
+	int i;
+	int j;
+	
+	for (i = 0; i < (2*length-1); i++) {
+		for (j = 0; j < length; j++){
+			output[i] = inputA[j]*inputB[j-i];
+		}
+	}
+}
+
+void home_conv_f32 (float *inputA, float *inputB, int length, float *output) {
+	int i;
+	int j;
+	
+	for (i = 0; i < (2*length-1); i++) {
+		for (j = 0; j < length; j++){
+			output[i] = inputA[j]*inputB[i-j];
+		}
+	}
 }
--- Lab1_STM32F4Cube_Base_project/Objects/kalmanfilter.d
+++ Lab1_STM32F4Cube_Base_project/Objects/kalmanfilter.d
@@ -1,4 +1,5 @@
 .\objects\kalmanfilter.o: KalmanFilter.c
 .\objects\kalmanfilter.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
 .\objects\kalmanfilter.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\kalmanfilter.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
 .\objects\kalmanfilter.o: KalmanFilter.h
