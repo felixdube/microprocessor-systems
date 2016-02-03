13a0369e9b32f971acf30bae09f344eb4b6a051b
augustelalande <auguste.lalande@gmail.com>
2016-02-03 13:51:32 -0500

    Revert "nothing again"
    This reverts commit 4b0fcb585e3ee92b80a4219a2e7e8300dbb60ed2, reversing
    changes made to 94ab06b6d61ad19d7610fe5c5f77ff4a8fce1985.

--- Lab1_STM32F4Cube_Base_project/KalmanFilter.h
+++ Lab1_STM32F4Cube_Base_project/KalmanFilter.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef KALMANFILTER_H
 #define KALMANFILTER_H
 
@@ -23,29 +22,3 @@ void home_correlate_f32 (float *inputA, float *inputB, int length, float *output
 void home_conv_f32 (float *inputA, float *inputB, int length, float *output);
 
 #endif
-=======
-#ifndef KALMANFILTER_H
-#define KALMANFILTER_H
-
-typedef struct kalman_state {
-	float q; //process noise covariance
-	float r; //measurement noise covariance
-	float x; //estimated value
-	float p; //estimation error covariance
-	float k; //adapive Kalman filter gain
-} kalman_state;
-
-int Kalmanfilter_C(float* InputArray, float* OutputArray, struct kalman_state* kstate, int Length);
-
-void home_sub_f32 (float *inputA, float *inputB, float *output, int length);
-
-void home_std_f32 (float *input, int length, float *output);
-
-void home_mean_f32 (float *input, int length, float *output);
-
-void home_correlate_f32 (float *inputA, float *inputB, int length, float *output);
-
-void home_conv_f32 (float *inputA, float *inputB, int length, float *output);
-
-#endif
->>>>>>> 79ba520d274295a40a1a1dce4fc6e0d643e145f4
--- Lab1_STM32F4Cube_Base_project/Objects/kalmanfilter.d
+++ Lab1_STM32F4Cube_Base_project/Objects/kalmanfilter.d
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 .\objects\kalmanfilter.o: KalmanFilter.c
 .\objects\kalmanfilter.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
 .\objects\kalmanfilter.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
 .\objects\kalmanfilter.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
 .\objects\kalmanfilter.o: KalmanFilter.h
-=======
-.\objects\kalmanfilter.o: KalmanFilter.c
-.\objects\kalmanfilter.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-.\objects\kalmanfilter.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\objects\kalmanfilter.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
-.\objects\kalmanfilter.o: KalmanFilter.h
->>>>>>> 79ba520d274295a40a1a1dce4fc6e0d643e145f4
