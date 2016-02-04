5cc24728321922811c92c615c06cdb328be124e1
augustelalande <auguste.lalande@gmail.com>
2016-02-04 17:41:19 -0500

    remove statisics prototypes from kalmanfilter.h

--- Lab1_STM32F4Cube_Base_project/KalmanFilter.h
+++ Lab1_STM32F4Cube_Base_project/KalmanFilter.h
@@ -11,14 +11,4 @@ typedef struct kalman_state {
 
 int Kalmanfilter_C(float* InputArray, float* OutputArray, struct kalman_state* kstate, int Length);
 
-void home_sub_f32 (float *inputA, float *inputB, float *output, int length);
-
-void home_std_f32 (float *input, int length, float *output);
-
-void home_mean_f32 (float *input, int length, float *output);
-
-void home_correlate_f32 (float *inputA, float *inputB, int length, float *output);
-
-void home_conv_f32 (float *inputA, float *inputB, int length, float *output);
-
 #endif
