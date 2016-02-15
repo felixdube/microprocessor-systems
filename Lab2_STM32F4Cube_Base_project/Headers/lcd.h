50da880206c6f8f676c37c483277f13b6976670e
augustelalande <auguste.lalande@gmail.com>
2016-02-14 21:30:38 -0500

    fix weird indent on lcd.h

--- Lab2_STM32F4Cube_Base_project/Headers/lcd.h
+++ Lab2_STM32F4Cube_Base_project/Headers/lcd.h
@@ -7,32 +7,31 @@
 	* Date							 : February, 2016
   ******************************************************************************
   */
-	
-	#ifndef LCD_H
-	#define LCD_H
-	
-	#define RS 	GPIO_PIN_2
-	#define RW 	GPIO_PIN_4
-	#define E 	GPIO_PIN_5
-	#define DB0 GPIO_PIN_6
-	#define DB1 GPIO_PIN_7
-	#define DB2 GPIO_PIN_8
-	#define DB3 GPIO_PIN_9
-	#define DB4 GPIO_PIN_10
-	#define DB5 GPIO_PIN_11
-	#define DB6 GPIO_PIN_12
-	#define DB7 GPIO_PIN_13
 
-	void initLCD(void);
-	void enable(void);
-	void LCD_WriteChar(char c);
-	void LCD_WriteString(char * string);
-	void clearDisplay(void);
-	void returnHome(void);
-	void inputMode(void);
-	void turnOn(void);
-	void functionSet(void);
-	void entryMode(void);
-	
-	#endif
-	
+#ifndef LCD_H
+#define LCD_H
+
+#define RS 	GPIO_PIN_2
+#define RW 	GPIO_PIN_4
+#define E 	GPIO_PIN_5
+#define DB0 GPIO_PIN_6
+#define DB1 GPIO_PIN_7
+#define DB2 GPIO_PIN_8
+#define DB3 GPIO_PIN_9
+#define DB4 GPIO_PIN_10
+#define DB5 GPIO_PIN_11
+#define DB6 GPIO_PIN_12
+#define DB7 GPIO_PIN_13
+
+void initLCD(void);
+void enable(void);
+void LCD_WriteChar(char c);
+void LCD_WriteString(char * string);
+void clearDisplay(void);
+void returnHome(void);
+void inputMode(void);
+void turnOn(void);
+void functionSet(void);
+void entryMode(void);
+
+#endif
