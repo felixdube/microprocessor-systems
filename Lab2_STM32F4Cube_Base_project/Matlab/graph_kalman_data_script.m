5a87cd7049f3d7903bd7e640d39e2f006a68154d
Juan Morency Trudel <juan-m-t@hotmail.com>
2016-02-16 11:14:36 -0500

    made the matlab even better

--- Lab2_STM32F4Cube_Base_project/Matlab/graph_kalman_data_script.m
+++ Lab2_STM32F4Cube_Base_project/Matlab/graph_kalman_data_script.m
@@ -4,7 +4,7 @@ s = size(data);
 number = 1:1:s;
 dataformatted = [number', data];
 str = sprintf('Kalman filter r = %f, q = %f, x = %f, p = %f, k = %f',dataformatted(1,3),dataformatted(1,4), dataformatted(1,5), dataformatted(1,6),dataformatted(1,7));
-figure;
+figure('units','normalized','outerposition',[0 0 1 1])
 subplot(2,1,1),
 plot(dataformatted(:,1),400 * dataformatted(:,2)*3/4096 - 279,dataformatted(:,1),400 * dataformatted(:,5)*3/4096 - 279);
 %plot(dataformatted(:,1),dataformatted(:,2),dataformatted(:,1),dataformatted(:,5));
@@ -12,7 +12,7 @@ title(str,'fontsize',18);
 xlabel('measurement #','fontsize',14);
 ylabel('Temperature in degree C','fontsize',14);
 legend('Input Data','Output Data');
-
+axis([0 500 35 45]);
 data = importdata('data_good_parameter.txt');
 s = size(data);
 number = 1:1:s;
@@ -25,6 +25,7 @@ title(str,'fontsize',18);
 xlabel('measurement #','fontsize',14);
 ylabel('Temperature in degree C','fontsize',14);
 legend('Input Data','Output Data');
+axis([0 500 35 45]);
 % 
 % data = importdata('data2.txt');
 % s = size(data);
@@ -38,3 +39,4 @@ legend('Input Data','Output Data');
 % xlabel('measurement #','fontsize',14);
 % ylabel('Temperature in degree C','fontsize',14);
 % legend('Input Data','Output Data');
+% axis([0 500 35 45]);
