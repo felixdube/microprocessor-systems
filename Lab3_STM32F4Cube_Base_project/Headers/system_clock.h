4b87ae2025cf5d0fa94398bbc41eefe39ca5a027
Felix Dube <felix.dube@mail.mcgill.ca>
2016-02-24 17:38:34 -0500

    Extract system clock

---
+++ Lab3_STM32F4Cube_Base_project/Headers/system_clock.h
@@ -0,0 +1,11 @@
+/**
+  ******************************************************************************
+  * File Name          : system_clock.h
+  * Description        : Header file for system_clock.c
+	* Author						 : Ashraf Suyyagh
+	* Version            : 1.0.0
+	* Date							 : January 14th, 2016
+  ******************************************************************************
+  */
+	
+void SystemClock_Config(void);
