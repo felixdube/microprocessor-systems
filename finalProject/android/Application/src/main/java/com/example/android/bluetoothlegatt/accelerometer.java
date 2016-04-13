47ab0e9bfadde64e1a0168acedcd60e5fe4276ff
Felix Dube <felix.dube@mail.mcgill.ca>
2016-04-12 20:22:57 -0400

    Add Update for graphs

--- finalProject/android/Application/src/main/java/com/example/android/bluetoothlegatt/accelerometer.java
+++ finalProject/android/Application/src/main/java/com/example/android/bluetoothlegatt/accelerometer.java
@@ -15,54 +15,27 @@ import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 
+
 public class accelerometer extends AppCompatActivity {
 
+    int[] timePitch = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+    int[] pointsPitch = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    GraphView graph_pitch = (GraphView) findViewById(R.id.graph_pitch);
+
+    int[] timeRoll = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+    int[] pointsRoll = {0,0,0,0,0,0,0,0,0,0};
+    GraphView graph_roll = (GraphView) findViewById(R.id.graph_roll);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accelerometer);
 
-        GraphView graph_roll = (GraphView) findViewById(R.id.graph_roll);
-        LineGraphSeries<DataPoint> series_roll = new LineGraphSeries<DataPoint>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6),
-                new DataPoint(5, 1),
-                new DataPoint(6, 5),
-                new DataPoint(7, 3),
-                new DataPoint(8, 2),
-                new DataPoint(9, 6)
-        });
         graph_roll.removeAllSeries();
-        graph_roll.addSeries(series_roll);
         graph_roll.setTitle("Roll Angle Over Time");
 
-        GraphView graph_pitch = (GraphView) findViewById(R.id.graph_pitch);
-        LineGraphSeries<DataPoint> series_pitch = new LineGraphSeries<DataPoint>(new DataPoint[] {
-                new DataPoint(0, 4),
-                new DataPoint(1, 3),
-                new DataPoint(2, 7),
-                new DataPoint(3, 5),
-                new DataPoint(4, 3),
-                new DataPoint(5, 8),
-                new DataPoint(6, 2),
-                new DataPoint(7, 4),
-                new DataPoint(8, 3),
-                new DataPoint(9, 2)
-        });
         graph_pitch.removeAllSeries();
-        graph_pitch.addSeries(series_pitch);
         graph_pitch.setTitle("Pitch Angle Over Time");
-
-
-
-
-
-
-
 
     }
 
@@ -78,5 +51,89 @@ public class accelerometer extends AppCompatActivity {
 
     }
 
+    private void updatePointsPitch(int newPoint) {
+
+
+        //shift the data
+        int i;
+        for(i = 0; i< 19; i++) {
+            pointsPitch[i] = pointsPitch[i+1];
+        }
+        pointsPitch[19] = newPoint;
+
+        //increment the time
+        for(i = 0; i< 20; i++){
+            timePitch[i] += 1;
+        }
+
+        LineGraphSeries<DataPoint> series_pitch = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(timePitch[0], pointsPitch[0]),
+                new DataPoint(timePitch[1], pointsPitch[1]),
+                new DataPoint(timePitch[2], pointsPitch[2]),
+                new DataPoint(timePitch[3], pointsPitch[3]),
+                new DataPoint(timePitch[4], pointsPitch[4]),
+                new DataPoint(timePitch[5], pointsPitch[5]),
+                new DataPoint(timePitch[6], pointsPitch[6]),
+                new DataPoint(timePitch[7], pointsPitch[7]),
+                new DataPoint(timePitch[8], pointsPitch[8]),
+                new DataPoint(timePitch[9], pointsPitch[9]),
+                new DataPoint(timePitch[10], pointsPitch[10]),
+                new DataPoint(timePitch[11], pointsPitch[11]),
+                new DataPoint(timePitch[12], pointsPitch[12]),
+                new DataPoint(timePitch[13], pointsPitch[13]),
+                new DataPoint(timePitch[14], pointsPitch[14]),
+                new DataPoint(timePitch[15], pointsPitch[15]),
+                new DataPoint(timePitch[16], pointsPitch[16]),
+                new DataPoint(timePitch[17], pointsPitch[17]),
+                new DataPoint(timePitch[18], pointsPitch[18]),
+                new DataPoint(timePitch[19], pointsPitch[19])
+        });
+        graph_pitch.removeAllSeries();
+        graph_pitch.addSeries(series_pitch);
+
+
+    }
+
+    private void updatePointsRoll(int newPoint) {
+
+
+        //shift the data
+        int i;
+        for(i = 0; i< 19; i++) {
+            pointsRoll[i] = pointsRoll[i+1];
+        }
+        pointsRoll[19] = newPoint;
+
+        //increment the time
+        for(i = 0; i< 20; i++){
+            timeRoll[i] += 1;
+        }
+
+        LineGraphSeries<DataPoint> series_roll = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(timeRoll[0], pointsRoll[0]),
+                new DataPoint(timeRoll[1], pointsRoll[1]),
+                new DataPoint(timeRoll[2], pointsRoll[2]),
+                new DataPoint(timeRoll[3], pointsRoll[3]),
+                new DataPoint(timeRoll[4], pointsRoll[4]),
+                new DataPoint(timeRoll[5], pointsRoll[5]),
+                new DataPoint(timeRoll[6], pointsRoll[6]),
+                new DataPoint(timeRoll[7], pointsRoll[7]),
+                new DataPoint(timeRoll[8], pointsRoll[8]),
+                new DataPoint(timeRoll[9], pointsRoll[9]),
+                new DataPoint(timeRoll[10], pointsRoll[10]),
+                new DataPoint(timeRoll[11], pointsRoll[11]),
+                new DataPoint(timeRoll[12], pointsRoll[12]),
+                new DataPoint(timeRoll[13], pointsRoll[13]),
+                new DataPoint(timeRoll[14], pointsRoll[14]),
+                new DataPoint(timeRoll[15], pointsRoll[15]),
+                new DataPoint(timeRoll[16], pointsRoll[16]),
+                new DataPoint(timeRoll[17], pointsRoll[17]),
+                new DataPoint(timeRoll[18], pointsRoll[18]),
+                new DataPoint(timeRoll[19], pointsRoll[19])
+        });
+        graph_roll.removeAllSeries();
+        graph_roll.addSeries(series_roll);
+
+    }
 
 }
--- finalProject/android/Application/src/main/java/com/example/android/bluetoothlegatt/temperature.java
+++ finalProject/android/Application/src/main/java/com/example/android/bluetoothlegatt/temperature.java
@@ -11,27 +11,17 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class temperature extends AppCompatActivity {
 
+    int[] timeTemp = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+    int[] pointsTemp = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    GraphView graph_temp = (GraphView) findViewById(R.id.graph_temp);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.temperature);
 
-        GraphView graph_temp = (GraphView) findViewById(R.id.graph_temp);
-        LineGraphSeries<DataPoint> series_temp = new LineGraphSeries<DataPoint>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6),
-                new DataPoint(5, 1),
-                new DataPoint(6, 5),
-                new DataPoint(7, 3),
-                new DataPoint(8, 2),
-                new DataPoint(9, 6)
-        });
         graph_temp.removeAllSeries();
-        graph_temp.addSeries(series_temp);
         graph_temp.setTitle("Temperature Over Time");
 
 
@@ -48,6 +38,48 @@ public class temperature extends AppCompatActivity {
     }
 
     private void refreshData() {
+
+    }
+
+    private void updatePointsTemp(int newPoint) {
+        
+        //shift the data
+        int i;
+        for(i = 0; i< 19; i++) {
+            pointsTemp[i] = pointsTemp[i+1];
+        }
+        pointsTemp[19] = newPoint;
+
+        //increment the time
+        for(i = 0; i< 20; i++){
+            timeTemp[i] += 1;
+        }
+
+        LineGraphSeries<DataPoint> series_temp = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(timeTemp[0], pointsTemp[0]),
+                new DataPoint(timeTemp[1], pointsTemp[1]),
+                new DataPoint(timeTemp[2], pointsTemp[2]),
+                new DataPoint(timeTemp[3], pointsTemp[3]),
+                new DataPoint(timeTemp[4], pointsTemp[4]),
+                new DataPoint(timeTemp[5], pointsTemp[5]),
+                new DataPoint(timeTemp[6], pointsTemp[6]),
+                new DataPoint(timeTemp[7], pointsTemp[7]),
+                new DataPoint(timeTemp[8], pointsTemp[8]),
+                new DataPoint(timeTemp[9], pointsTemp[9]),
+                new DataPoint(timeTemp[10], pointsTemp[10]),
+                new DataPoint(timeTemp[11], pointsTemp[11]),
+                new DataPoint(timeTemp[12], pointsTemp[12]),
+                new DataPoint(timeTemp[13], pointsTemp[13]),
+                new DataPoint(timeTemp[14], pointsTemp[14]),
+                new DataPoint(timeTemp[15], pointsTemp[15]),
+                new DataPoint(timeTemp[16], pointsTemp[16]),
+                new DataPoint(timeTemp[17], pointsTemp[17]),
+                new DataPoint(timeTemp[18], pointsTemp[18]),
+                new DataPoint(timeTemp[19], pointsTemp[19])
+        });
+        graph_temp.removeAllSeries();
+        graph_temp.addSeries(series_temp);
+
 
     }
 
